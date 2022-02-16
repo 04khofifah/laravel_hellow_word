66af794f65370a33e872b6a07be2b74ec28617ce
04khofifah <2041720119@student.polinema.ac.id>
2022-02-16 19:54:04 +0700

    commed class antialised

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -22,12 +22,12 @@
     </head>
 
     <body class="antialiased">
-        <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
+        <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center  sm:pt-0">
             <div class="ml-4 text-lg leading-7 font-semibold"><h1 class="text-gray-900 dark:text-white"> Khofifah Amanda</h1></div>
         </div>
     </body>
 
-    <body class="antialiased">
+    <!-- <body class="antialiased">
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
             @if (Route::has('login'))
                 <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
@@ -135,5 +135,5 @@
                 </div>
             </div>
         </div>
-    </body>
+    </body> -->
 </html>
